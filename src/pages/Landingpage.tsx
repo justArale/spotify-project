84745e1b19cc46b9e8fcaec5af77dcb9ed0003b2
justArale <just1arale@gmail.com>
2024-07-31 10:19:03 +0200

    set landingpage

---
+++ src/pages/Landingpage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import About from "../components/About";
+
+function Landingpage() {
+  return (
+    <>
+      <Header />
+      <About />
+    </>
+  );
+}
+
+export default Landingpage;
