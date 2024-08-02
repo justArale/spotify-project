c2601a7aa2bebd63b61eeb42f7f7ae192b6f18ef
justArale <just1arale@gmail.com>
2024-08-02 14:41:41 +0200

    content for about-part

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const About: React.FC = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div>
+        <div>1</div>
+        <p>Start with any Artist you love and choose the right mood.</p>
+      </div>
+      <div>
+        <div>2</div>
+        <p>Based on your choice we will generate the perfect playlist.</p>
+      </div>
+      <div>
+        <div>3</div>
+        <p>Preview listen tracks and save the playlist to your library.</p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
