3eb6b3df55ddfa818ef3b9647e602a208ad48eff
justArale <just1arale@gmail.com>
2024-08-03 18:27:37 +0200

    added footer.css file and styled the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,29 @@
-// import "./Footer.css";
+import "./Footer.css";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="">
-      <p className="">
-        <a href="https://github.com/justArale/spotify-project ">
-          Spotify Project
-        </a>{" "}
-        made with ❤️ {"   "}by{"  "}
-        <a href="https://github.com/justArale">Arale</a>
-      </p>
-    </footer>
+    <div className="footer">
+      <div className="creditWrapper">
+        <p className="meta">
+          <a
+            href="https://github.com/justArale/spotify-project "
+            target="_blank"
+            className="blackFont"
+          >
+            Spotify Project
+          </a>{" "}
+          made with ❤️ {"   "}by{"  "}
+          <a
+            href="https://github.com/justArale"
+            target="_blank"
+            className="blackFont"
+          >
+            Arale
+          </a>
+        </p>
+      </div>
+    </div>
   );
 };
 
