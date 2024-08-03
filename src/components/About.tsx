8d74699d7c7bb806b28de2fc03ce94f000da157e
justArale <just1arale@gmail.com>
2024-08-03 18:51:58 +0200

    styling of about-the-project-part

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,19 +1,26 @@
+import "./About.css";
 import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div>
-      <div>
-        <div>1</div>
-        <p>Start with any Artist you love and choose the right mood.</p>
+    <div className="boxWrapper">
+      <div className="descriptionBox">
+        <div className="numberFont counter">1</div>
+        <p className="mainFontSmall">
+          Start with any Artist you love and choose the right mood.
+        </p>
       </div>
-      <div>
-        <div>2</div>
-        <p>Based on your choice we will generate the perfect playlist.</p>
+      <div className="descriptionBox">
+        <div className="numberFont counter">2</div>
+        <p className="mainFontSmall">
+          Based on your choice we will generate the perfect playlist.
+        </p>
       </div>
-      <div>
-        <div>3</div>
-        <p>Preview listen tracks and save the playlist to your library.</p>
+      <div className="descriptionBox">
+        <div className="numberFont counter">3</div>
+        <p className="mainFontSmall">
+          Preview listen tracks and save the playlist to your library.
+        </p>
       </div>
     </div>
   );
