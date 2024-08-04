94a3bb9a02cb868e72ef13aa6c0f2fc69f21c050
justArale <just1arale@gmail.com>
2024-08-04 14:33:29 +0200

    added className to style the Landingpage

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -1,13 +1,14 @@
+import "../components/Landingpage.css";
 import React from "react";
 import Header from "../components/Header";
 import About from "../components/About";
 
 const Landingpage: React.FC = () => {
   return (
-    <>
+    <div className="landingPageWrapper">
       <Header />
       <About />
-    </>
+    </div>
   );
 };
 
