34c127738c6c7340f09451ea49746d1992ec7e37
justArale <just1arale@gmail.com>
2024-08-04 14:35:36 +0200

    styling for header on landingpage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
+import "./Header.css";
 import React from "react";
 import CreateButton from "./CreateButton";
-import CoverImage from "../assets/Cover.png";
 
 const Header: React.FC = () => {
   return (
-    <div>
-      <img
-        src={CoverImage}
-        alt="Image with a yellow background and a headphone on the right side"
-      />
-      <h1>Header</h1>
-      <CreateButton />
+    <div className="headerWraper">
+      <div className="headerContentWrapper">
+        <div className="headerContent">
+          <p className="headline">Spotify Project</p>
+          <h1 className="pageTitle">
+            Generate Playlists <br /> by{" "}
+            <span className="resultTitle mainColor">Mood</span> and{" "}
+            <span className="resultTitle mainColor">Artists</span>
+            <br /> with Spotify.
+          </h1>
+          <CreateButton />
+        </div>
+      </div>
     </div>
   );
 };
