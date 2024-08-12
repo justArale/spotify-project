2818f02c37bcda8d43f5bf8e782bd86aab0e6974
justArale <just1arale@gmail.com>
2024-08-12 23:00:35 +0200

    imported matching css file and styled component with setted classes

--- src/components/SelectedArtist.tsx
+++ src/components/SelectedArtist.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SelectedArtist.css";
 import CreateButton from "./CreateButton";
 
 const SelectedArtist: React.FC = () => {
@@ -7,21 +8,28 @@ const SelectedArtist: React.FC = () => {
   const getArtistImage = localStorage.getItem("artistImage");
 
   return (
-    <div>
-      <div>{getArtistImage && <img src={getArtistImage} alt="Artist" />}</div>
-      <div>
-        <div>
-          <div>
-            {getArtistImage && <img src={getArtistImage} alt="Artist" />}
+    <div className="resultHeader">
+      <div
+        className="blurryBackground"
+        style={{
+          backgroundImage: `url(${getArtistImage})`,
+        }}
+      ></div>
+      <div className="resultHeaderContentWrapper">
+        <div className="resultPlaylistContent">
+          <div className="resultPlaylistArtistImageWrapper">
+            {getArtistImage && (
+              <img src={getArtistImage} alt="Artist" className="artistImage" />
+            )}
           </div>
-          <div>
-            <h3>Your new playlist</h3>
-            <h2>
+          <div className="resultPlaylistText">
+            <h3 className="headline whiteFont">Your New Playlist</h3>
+            <h2 className="resultTitle whiteFont">
               {getMoodInput} with {getArtistInput}
             </h2>
           </div>
         </div>
-        <div>
+        <div className="actionButtons">
           <button>Save to your spotify</button>
           <CreateButton />
         </div>
