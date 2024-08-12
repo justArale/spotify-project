26c8df669fe4958d6881054046391c7ec323a126
justArale <just1arale@gmail.com>
2024-08-12 21:23:11 +0200

    render the result header with selected artist component and set audio play function, set props definition

--- src/pages/Resultpage.tsx
+++ src/pages/Resultpage.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import SelectedArtist from "../components/SelectedArtist";
 
-const Resultpage: React.FC = () => {
+interface ResultProps {
+  playlistData: any;
+}
+
+const Resultpage: React.FC<ResultProps> = ({ playlistData }) => {
+  const getPlaylistData = localStorage.getItem("playlistData");
+  const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(
+    null
+  );
+
+  const playAudio = (url: string) => {
+    if (currentAudio) {
+      currentAudio.pause();
+    }
+    const audio = new Audio(url);
+    audio.play();
+    setCurrentAudio(audio);
+  };
+
   return (
     <div>
       <div>
         <SelectedArtist />
       </div>
       <h5>Your playlist will be displayed here ✌️</h5>
+      {/* <ul>
+        {playlistData.map((track: any) => (
+          <li key={track.id}>
+            {track.name} - {track.artists[0].name}
+            <button onClick={() => playAudio(track.preview_url)}>
+              Play Preview
+            </button>
+          </li>
+        ))}
+      </ul> */}
     </div>
   );
 };
