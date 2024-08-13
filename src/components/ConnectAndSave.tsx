d5d94f60ca13a936aad2c63567ee57a3d1410e5f
justArale <just1arale@gmail.com>
2024-08-13 14:02:21 +0200

    imported auth.context to use the  context instead of local storage and removed the outsourced function to connect to sp otify, also updated the trackURIs and called the function to close the overlay if the playlist is created and the tracks are added successfully

--- src/components/ConnectAndSave.tsx
+++ src/components/ConnectAndSave.tsx
@@ -4,6 +4,7 @@ import "./ConnectAndSave.css";
 import connectIcon from "../assets/icons/connect.png";
 import addIcon from "../assets/icons/add.svg";
 import { CollectContext } from "../context/collectData.context";
+import { AuthContext } from "../context/auth.context"; // Importiere den AuthContext
 
 const CLIENT_ID: string = import.meta.env.VITE_CLIENT_ID as string;
 const SPOTIFY_AUTHORIZE_ENDPOINT: string =
@@ -17,22 +18,6 @@ const SCOPES: string[] = [
   "playlist-modify-public",
 ];
 const SCOPES_URL_PARAMS: string = SCOPES.join(SPACE);
-
-const getReturnedParamsFromSpotifyAuth = (
-  hash: string
-): Record<string, string> => {
-  const stringAfterHashtag = hash.substring(1);
-  const paramsInUrl = stringAfterHashtag.split("&");
-  const paramsSplitUp = paramsInUrl.reduce(
-    (acc: Record<string, string>, currValue: string) => {
-      const [key, value] = currValue.split("=");
-      acc[key] = value;
-      return acc;
-    },
-    {}
-  );
-  return paramsSplitUp;
-};
 
 const ConnectAndSave: React.FC = () => {
   const [isOverlayOpen, setIsOverlayOpen] = useState<boolean>(false);
@@ -65,39 +50,26 @@ interface ConnectAndSaveOverlayProps {
 const ConnectAndSaveOverlay: React.FC<ConnectAndSaveOverlayProps> = ({
   onClose,
 }) => {
-  const [token, setToken] = useState<string | null>(
-    localStorage.getItem("accessToken")
-  );
+  const { accessToken } = useContext(AuthContext);
   const [userId, setUserId] = useState<string>("");
   const [playlistId, setPlaylistId] = useState<string>("");
 
   const PLAYLIST_ENDPOINT = `https://api.spotify.com/v1/users/${userId}/playlists`;
   const TRACK_ENDPOINT = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
   const USER_ENDPOINT = `https://api.spotify.com/v1/me`;
-  const { choosenArtistName, choosenMood } = useContext(CollectContext);
-
-  useEffect(() => {
-    if (window.location.hash) {
-      const { access_token, expires_in, token_type } =
-        getReturnedParamsFromSpotifyAuth(window.location.hash);
-      localStorage.clear();
-      localStorage.setItem("accessToken", access_token);
-      localStorage.setItem("expiresIn", expires_in);
-      localStorage.setItem("tokenType", token_type);
-      setToken(access_token);
-    }
-  }, []);
+  const { choosenArtistName, choosenMood, playlistData } =
+    useContext(CollectContext);
 
   const handleLogin = () => {
     window.location.href = `${SPOTIFY_AUTHORIZE_ENDPOINT}client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAMS}&response_type=token&show_dialog=true`;
   };
 
   const getUserInfo = () => {
-    if (!token) return;
+    if (!accessToken) return;
     axios
       .get(USER_ENDPOINT, {
         headers: {
-          Authorization: "Bearer " + token,
+          Authorization: "Bearer " + accessToken,
         },
       })
       .then((response) => {
@@ -109,24 +81,24 @@ const ConnectAndSaveOverlay: React.FC<ConnectAndSaveOverlayProps> = ({
   };
 
   useEffect(() => {
-    if (token) {
+    if (accessToken) {
       getUserInfo();
     }
-  }, [token]);
+  }, [accessToken]);
 
   const createPlaylist = async () => {
-    if (!token || !userId) return;
+    if (!accessToken || !userId) return;
     try {
       const response = await axios.post(
         PLAYLIST_ENDPOINT,
         {
-          name: `${choosenMood} ${choosenArtistName} Playlist`,
+          name: `${choosenMood} with ${choosenArtistName} Playlist`,
           description: "Created with spotify-project app",
           public: true,
         },
         {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${accessToken}`,
             "Content-Type": "application/json",
           },
         }
@@ -143,7 +115,12 @@ const ConnectAndSaveOverlay: React.FC<ConnectAndSaveOverlayProps> = ({
       if (!playlistId) return;
 
       // Placeholder for actual track URIs
-      const trackURIs: string[] = [];
+      const trackURIs = playlistData.map((track) => track.uri);
+
+      if (trackURIs.length === 0) {
+        console.error("No track URIs available to add to playlist");
+        return;
+      }
 
       try {
         await axios.post(
@@ -154,21 +131,22 @@ const ConnectAndSaveOverlay: React.FC<ConnectAndSaveOverlayProps> = ({
           },
           {
             headers: {
-              Authorization: `Bearer ${token}`,
+              Authorization: `Bearer ${accessToken}`,
               "Content-Type": "application/json",
             },
           }
         );
         console.log("Tracks added successfully!");
+        onClose();
       } catch (error) {
         console.log("Error adding tracks to playlist:", error);
       }
     };
     setTracksToPlaylist();
-  }, [playlistId, token]);
+  }, [playlistId, accessToken]);
 
   const handleConnectAndSave = () => {
-    if (token) {
+    if (accessToken) {
       createPlaylist();
     } else {
       handleLogin();
