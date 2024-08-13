9fd3bcba0d2adc859bb12377343d00fa03b84d10
justArale <just1arale@gmail.com>
2024-08-13 14:37:41 +0200

    moved the playlist function and render part in an seperate component and render that component instead of directly

---
+++ src/components/Playlist.tsx
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from "react";
+import { CollectContext } from "../context/collectData.context";
+
+const Playlist: React.FC = () => {
+  const { playlistData } = useContext(CollectContext);
+  const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(
+    null
+  );
+
+  const playAudio = (url: string) => {
+    if (currentAudio) {
+      currentAudio.pause();
+    }
+    const audio = new Audio(url);
+    audio.play();
+    setCurrentAudio(audio);
+  };
+
+  return (
+    <div>
+      <ul>
+        {playlistData.map((track: any) => (
+          <li key={track.id}>
+            {track.name} - {track.artists[0].name}
+            <button onClick={() => playAudio(track.preview_url)}>
+              Play Preview
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Playlist;
--- src/pages/Resultpage.tsx
+++ src/pages/Resultpage.tsx
@@ -1,35 +1,12 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 import SelectedArtist from "../components/SelectedArtist";
-import { CollectContext } from "../context/collectData.context";
+import Playlist from "../components/Playlist";
 
 const Resultpage: React.FC = ({}) => {
-  const { playlistData } = useContext(CollectContext);
-  const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(
-    null
-  );
-
-  const playAudio = (url: string) => {
-    if (currentAudio) {
-      currentAudio.pause();
-    }
-    const audio = new Audio(url);
-    audio.play();
-    setCurrentAudio(audio);
-  };
-
   return (
     <div className="landingPageWrapper">
       <SelectedArtist />
-      <ul>
-        {playlistData.map((track: any) => (
-          <li key={track.id}>
-            {track.name} - {track.artists[0].name}
-            <button onClick={() => playAudio(track.preview_url)}>
-              Play Preview
-            </button>
-          </li>
-        ))}
-      </ul>
+      <Playlist />
     </div>
   );
 };
