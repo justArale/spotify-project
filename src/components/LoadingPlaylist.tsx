f96ab38640cd5c1fb1ea3f154c2d1fa8443df4fa
justArale <just1arale@gmail.com>
2024-08-13 11:38:07 +0200

    removed import of useState, because of unuse

--- src/components/LoadingPlaylist.tsx
+++ src/components/LoadingPlaylist.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import loadingIcon from "../assets/icons/loading.svg";
