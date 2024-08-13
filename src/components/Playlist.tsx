9272cdca42bb4a1fda7f733cb5aeef0b75c87fea
justArale <just1arale@gmail.com>
2024-08-13 17:58:22 +0200

    importet play and pause icon, set play and pause function and started working on loading cicle

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,20 +1,34 @@
 import React, { useContext, useState } from "react";
 import { CollectContext } from "../context/collectData.context";
 import "./Playlist.css";
+import playIcon from "../assets/icons/play.svg";
+import pauseIcon from "../assets/icons/pause.svg";
 
 const Playlist: React.FC = () => {
   const { playlistData } = useContext(CollectContext);
   const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(
     null
   );
+  const [currentTrackUrl, setCurrentTrackUrl] = useState<string | null>(null);
 
   const playAudio = (url: string) => {
     if (currentAudio) {
-      currentAudio.pause();
+      if (currentTrackUrl === url) {
+        // If the same track is clicked, toggle play/pause
+        currentAudio.pause();
+        setCurrentTrackUrl(null);
+        setCurrentAudio(null);
+        return;
+      } else {
+        // Stop the currently playing track
+        currentAudio.pause();
+      }
     }
+    // Play the new track
     const audio = new Audio(url);
     audio.play();
     setCurrentAudio(audio);
+    setCurrentTrackUrl(url);
   };
 
   return (
@@ -26,10 +40,27 @@ const Playlist: React.FC = () => {
             className="track"
             onClick={() => playAudio(track.preview_url)}
           >
-            <p className="trackNumber">{index + 1}.</p>
-            <div>
+            <div className="trackNumberWrapper">
+              <p className="trackNumber">{index + 1}.</p>
+            </div>
+            <div className="contentText">
               <p className="trackName"> {track.name} </p>
               <p className="trackArtistName">{track.artists[0].name}</p>
+            </div>
+            <div className="trackNumberWrapper">
+              <img
+                src={
+                  currentTrackUrl === track.preview_url && !currentAudio?.paused
+                    ? pauseIcon
+                    : playIcon
+                }
+                alt={currentTrackUrl === track.preview_url ? "Pause" : "Play"}
+                // className={`progress-circle ${
+                //   currentTrackUrl === track.preview_url && !currentAudio?.paused
+                //     ? ""
+                //     : "paused"
+                // }`}
+              />
             </div>
           </li>
         ))}
