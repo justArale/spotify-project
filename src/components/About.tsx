6cb6c03960ca52c35f700e15accff9d35d3256c1
justArale <just1arale@gmail.com>
2024-09-27 13:14:14 +0200

    update about page to align with the new design #17

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,23 +4,26 @@ import React from "react";
 const About: React.FC = () => {
   return (
     <div className="boxWrapper">
-      <div className="descriptionBox">
-        <div className="numberFont counter">1</div>
-        <p className="mainFontSmall">
-          Start with any Artist you love and choose the right mood.
-        </p>
-      </div>
-      <div className="descriptionBox">
-        <div className="numberFont counter">2</div>
-        <p className="mainFontSmall">
-          Based on your choice we will generate the perfect playlist.
-        </p>
-      </div>
-      <div className="descriptionBox">
-        <div className="numberFont counter">3</div>
-        <p className="mainFontSmall">
-          Preview listen tracks and save the playlist to your library.
-        </p>
+      <p className="bodyText">How it works</p>
+      <div className="descriptionBoxWrapper">
+        <div className="descriptionBox">
+          <div className="numberFont counter">1</div>
+          <p className="subHeadline">
+            Start with any Artist you love and choose the right mood.
+          </p>
+        </div>
+        <div className="descriptionBox">
+          <div className="numberFont counter">2</div>
+          <p className="subHeadline">
+            Based on your choice we will generate the perfect playlist.
+          </p>
+        </div>
+        <div className="descriptionBox">
+          <div className="numberFont counter">3</div>
+          <p className="subHeadline">
+            Preview listen tracks and save the playlist to your library.
+          </p>
+        </div>
       </div>
     </div>
   );
