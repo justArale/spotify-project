f9790a3e99c499e85fffdc651d6ad7f7fa279cb1
justArale <just1arale@gmail.com>
2024-09-28 17:49:33 +0200

    add css class for the resultpage to fit with the new design #17

--- src/pages/Resultpage.tsx
+++ src/pages/Resultpage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SelectedArtist from "../components/SelectedArtist";
 import Playlist from "../components/Playlist";
+import "../components/Resultpage.css";
 
 const Resultpage: React.FC = ({}) => {
   return (
-    <div className="landingPageWrapper">
+    <div className="landingPageWrapper resultPageWrapper">
       <SelectedArtist />
       <Playlist />
     </div>
