d2e49d23970f2c84670ba4cb4bde3860efc20faf
justArale <just1arale@gmail.com>
2024-10-01 20:57:43 +0200

    Changed Title and Subheader text, add spotify logo from library #14 #17

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
 import "./Header.css";
 import React from "react";
 import CreateButton from "./CreateButton";
+import { SpotifyLogo } from "@just1arale/icons";
 
 const Header: React.FC = () => {
   return (
     <div className="headerWraper">
       <div className="headerContentWrapper">
+        <p className="bodyText">
+          made for <SpotifyLogo width="16" height="16" />
+        </p>
         <div className="headerContent">
-          <p className="headline">Spotify Project</p>
-          <h1 className="pageTitle">
-            Generate Playlists <br /> by{" "}
-            <span className="resultTitle mainColor">Mood</span> and{" "}
-            <span className="resultTitle mainColor">Artists</span>
-            <br /> with Spotify.
-          </h1>
-          <CreateButton />
+          <h1 className="pageTitle">Moodly</h1>
+          <p className="headline">
+            Generate Spotify playlists based on mood and artists
+          </p>
         </div>
+        <CreateButton />
       </div>
     </div>
   );
