8ed92fa34953d53ad12df190f587560cf7d516eb
justArale <just1arale@gmail.com>
2024-10-02 10:19:06 +0200

    Rearrenge headercontent for mobile screens #17

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,17 @@ import { SpotifyLogo } from "@just1arale/icons";
 
 const Header: React.FC = () => {
   return (
-    <div className="headerWraper">
-      <div className="headerContentWrapper">
-        <p className="bodyText">
-          made for <SpotifyLogo width="16" height="16" />
+    <div className="headerContentWrapper">
+      <p className="bodyText">
+        made for <SpotifyLogo width="16" height="16" />
+      </p>
+      <div className="headerContent">
+        <h1 className="pageTitle">Moodly</h1>
+        <p className="headline">
+          Generate Spotify playlists based on mood and artists
         </p>
-        <div className="headerContent">
-          <h1 className="pageTitle">Moodly</h1>
-          <p className="headline">
-            Generate Spotify playlists based on mood and artists
-          </p>
-        </div>
-        <CreateButton />
       </div>
+      <CreateButton />
     </div>
   );
 };
